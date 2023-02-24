98dc81060909984ea63e4b782f872a85181131fa
Jeongjae Lee <wjdwo0413@naver.com>
2023-02-24 18:59:14 +0900

    나만의 책장 앱 만들기(화면 구성하기1)

--- 나만의 책장/나만의 책장/Scene/MyBookcase/MyBookcasePresenter.swift
+++ 나만의 책장/나만의 책장/Scene/MyBookcase/MyBookcasePresenter.swift
@@ -8,15 +8,47 @@
 import Foundation
 import UIKit
 
-protocol MyBookcaseProtocol {}
+//MARK: - MyBookcase Protocol
+protocol MyBookcaseProtocol {
+    func setupNavigationBar()
+    func setupViews()
+}
 
-final class MyBookcasePresenter {
+//MARK: - MyBookcasePresenter Class
+final class MyBookcasePresenter: NSObject {
     
     
+    //프로퍼티
     private let viewController: MyBookcaseProtocol
-    
     init(viewController: MyBookcaseProtocol) {
         self.viewController = viewController
+    }
+    
+    //뷰가 생성될 때 호출될 함수
+    func viewDidLoad() {
+        self.viewController.setupNavigationBar()
+        self.viewController.setupViews()
+    }
+    
+    
+}
+
+//MARK: - MyBookcasePresenter Extension
+extension MyBookcasePresenter: UITableViewDataSource {
+    
+    
+    //행의 갯수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    //셀 설정
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        
+        cell.textLabel?.text = "\(indexPath)"
+        
+        return cell
     }
     
     
--- 나만의 책장/나만의 책장/Scene/MyBookcase/MyBookcaseViewController.swift
+++ 나만의 책장/나만의 책장/Scene/MyBookcase/MyBookcaseViewController.swift
@@ -6,21 +6,60 @@
 //
 
 import UIKit
+import SnapKit
 
-class MyBookcaseViewController: UIViewController {
+//MARK: - MyBookcaseViewController Class
+final class MyBookcaseViewController: UIViewController {
 
     
+    //프로퍼티
     private lazy var presenter = MyBookcasePresenter(viewController: self)
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        
+        tableView.dataSource = self.presenter
+        
+        return tableView
+    }()
     
+    //뷰가 생성되었을 때
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        self.presenter.viewDidLoad()
     }
     
-
+    
 }
 
+//MARK: - MyBookcaseViewController Extension
 extension MyBookcaseViewController: MyBookcaseProtocol {
+    
+    
+    //네비게이션 컨트롤러 셋업
+    func setupNavigationBar() {
+        //네비게이션 타이틀
+        self.navigationItem.title = "나만의 책장"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        //네비게이션 우측 바 버튼 아이템
+        let rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: nil
+        )
+        self.navigationItem.rightBarButtonItem = rightBarButtonItem
+    }
+    
+    //뷰 컨트롤러  셋업
+    func setupViews() {
+        //테이블 뷰 삽입
+        self.view.addSubview(self.tableView)
+        
+        self.tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
     
 }
